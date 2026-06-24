f28c61cf866227eb0178a0b10b5619bb950bf71c
Kolawole, Omowunmi Esther <logosphile@yahoo.com>
2026-06-24 00:52:31 -0500

    initializing

---
+++ IDS_Lightwork/IDS_Lightwork/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  IDS_Lightwork
+//
+//  Created by KOLAWOLE ESTHER on 6/24/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
